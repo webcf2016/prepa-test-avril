1b3f5ca95c06c1e7521cc8453a01c8b93760d722
Mikhawa <michaeljpitz@gmail.com>
2016-04-11 11:07:19 +0200

    index merge

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
-<<<<<<< HEAD
  * User: webform
  * Date: 11/04/2016
  * Time: 10:49
  *
  * Contrôleur frontal
-=======
- * User: Stagiaire
- * Date: 11/04/2016
- * Time: 10:49
- *
- *
- * controlleur frontal
->>>>>>> 53967a7590ae13f64f5329d15ae33cb27b750344
+
  */
